317038623a045a2a2a093196fcc1c10bc437caf8
Reut <sasson.reut@gmail.com>
2026-04-26 11:12:35 +0300

    first commit

---
+++ Reut.java
@@ -0,0 +1,8 @@
+public class Reut {
+    public static void main(String[] args) {
+        System.out.println("Reut");
+        System.out.println("Reut");
+        System.out.println("Reut");
+        System.out.println("Reut");
+    }
+}
